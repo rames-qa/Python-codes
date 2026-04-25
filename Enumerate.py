fc84ee66cf3b4971492084eeaf54492567c5eb31
Ramesh Kumar K <rameshkumar93.68@gmail.com>
2026-04-25 17:18:06 +0530

    Add files via upload

---
+++ Enumerate.py
@@ -0,0 +1,9 @@
+marks = [12,56,32,98,12,45,1,4]
+#index = 0 # Without enumerate fuction
+for  index , mark in enumerate(marks):
+    print(mark)
+
+    if index == 3:
+        print("Ramesh, Awesome!")
+
+    index += 1
