7a9df06d196d5b50321a935e9c38965645e1439e
Ramesh Kumar K <rameshkumar93.68@gmail.com>
2026-04-28 15:58:16 +0530

    Refactor print statements and formatting

--- Snake-Water-Gun.py
+++ Snake-Water-Gun.py
@@ -1,16 +1,16 @@
 import random
-print("\n Game Logic (Result Matrix)\n")
-print("User \\ Computer\tSnake (s)\tWater (w)\tGun (g)")
-print("Snake (s)\t\tTie (0)\t\tWin (1)\t\tLose (-1)")
-print("Water (w)\t\tLose (-1)\tTie (0)\t\tWin (1)")
-print("Gun (g)\t\t\tWin (1)\t\tLose (-1)\tTie (0)\n")
+print("Game Logic (Result Matrix)")
+print("User\\Computer,Snake(s),Water(w),Gun(g)")
+print("Snake(s),Tie(0),Win(1),Lose(-1)")
+print("Water(w),Lose (-1,Tie(0),Win(1)")
+print("Gun(g),Win(1),Lose(-1),Tie(0)")
 options = ["s", "w", "g"]
 comp = random.choice(options)
-user = input("Snake (s), Water (w), Gun (g)\nEnter (s/w/g)= ").lower()
+user = input("Snake(s),Water(w),Gun(g)\nEnter (s/w/g)= ").lower()
 if user not in options:
     print("Invalid input!")
 else:
-    print(f"\nComputer: {comp}\nYou: {user}")
+    print(f"\nComputer:{comp}\nYou:{user}")
     if comp == user:
         print("It's a tie!")
     elif (user == "s" and comp == "w") or \
